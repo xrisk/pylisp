ef9ce758ebc5aa586d295b2bf3e31fba99f6484d
Rishav Kundu <rk@rishav.io>
2021-01-03 09:01:28 +0530

    Persist environment in lambdas

--- ex.py
+++ ex.py
@@ -11,12 +11,16 @@ class A:
 
 
 class Proc(A):
-    def __init__(self, names, body):
+    def __init__(self, env, names, body):
+        self.env = env
         self.names = names
         self.body = body
 
     def apply(self, env, *args):
-        new_env = env.copy()
+        # print(env)
+        new_env = self.env.copy()
+        for n, v in env.kv.items():
+            new_env.extend(n, v)
         for n, v in zip(self.names, args):
             new_env.extend(n, v)
         return self.body.eval(new_env)
@@ -35,13 +39,13 @@ class Id(A):
     def eval(self, env):
         return env[self]
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Int(A):
     def __init__(self, v: int):
         self.val = v
-
-    def eval(self, env):
-        return self
 
     def __str__(self):
         return str(self.val)
@@ -54,6 +58,9 @@ class StringLiteral(A):
 class AddOp(Proc):
     def __init__(self):
         pass
+
+    def __str__(self):
+        return "+"
 
     def apply(self, env, *args):
         x, y = args
@@ -76,6 +83,9 @@ class S(A):
             for item in self.body:
                 yield item
 
+    def __str__(self):
+        return f"({' '.join(str(x) for x in self.body)})"
+
     def eval(self, env: Env):
         if isinstance(self.body, A):
             return self.body
@@ -97,6 +107,9 @@ class Let(S):
             ext.extend(b[0], b[1].eval(env))
         return self.body.eval(ext)
 
+    def __str__(self):
+        return f"(let {self.binds} {self.body})"
+
 
 class Lambda(S):
     def __init__(self, names, body):
@@ -104,14 +117,17 @@ class Lambda(S):
         self.body = body
 
     def eval(self, env, *args):
-        ret = Proc(self.names, self.body)
+        ret = Proc(env, self.names, self.body)
         return ret
+
+    def __str__(self):
+        return f"(lambda {self.names} {self.body})"
 
 
 class Env:
     def __init__(self, kv=None):
         if kv:
-            self.kv = kv.clone()
+            self.kv = kv.copy()
         else:
             self.kv = {}
 
@@ -120,10 +136,17 @@ class Env:
         self.kv[key] = value
 
     def __getitem__(self, val):
-        return self.kv[val]
+        try:
+            return self.kv[val]
+        except KeyError as e:
+            print(f"{val} not found")
+            raise
 
     def copy(self):
         return Env(self.kv)
+
+    def __str__(self):
+        return str(self.kv)
 
 
 #   ((lambda (x) (lambda (x) x)) 5)
@@ -135,8 +158,14 @@ env = Env()
 
 # test2 = Let(S(S(Id("x"), Int(5)), S(Id("y"), Int(2))), S(AddOp(), Id("y"), Id("x")))
 
-test1 = S(Lambda(S(Id("x")), S(AddOp(), Int(2), Id("x"))), Int(2))
+# test1 = S(Lambda(S(Id("x")), S(AddOp(), Int(2), Id("x"))), Int(2))
 
 # test3
+
+test1 = S(
+    Let(S(S(Id("x"), Int(5))), Lambda(S(Id("z")), S(AddOp(), Id("x"), Id("z")))), Int(3)
+)
+
+print("input ast:", test1)
 
 print(test1.eval(env))
