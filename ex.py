a6389307675e00ca1bc89964c0356abd622ca418
Rishav Kundu <rk@rishav.io>
2021-01-03 07:30:10 +0530

    Implement let

--- ex.py
+++ ex.py
@@ -15,15 +15,21 @@ class Callable(A):
         raise Exception("not implemented!")
 
 
-class Identifier(A):
+class Id(A):
     def __init__(self, v):
         self.name = v
 
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
     def eval(self, env):
-        return env[self.name]
+        return env[self]
 
 
-class Integer(A):
+class Int(A):
     def __init__(self, v: int):
         self.val = v
 
@@ -42,13 +48,22 @@ class AddOp(Callable):
     def __init__(self):
         pass
 
-    def eval(self, env):
-        return self
-
     def apply(self, x, y):
-        assert isinstance(x, Integer)
-        assert isinstance(y, Integer)
-        return Integer(x.val + y.val)
+        assert isinstance(x, Int)
+        assert isinstance(y, Int)
+        return Int(x.val + y.val)
+
+
+class Let(Callable):
+    def __init__(self, binds, body):
+        self.binds = binds
+        self.body = body
+
+    def eval(self, env):
+        ext = env.copy()
+        for b in self.binds:
+            ext.extend(b[0], b[1].eval(env))
+        return self.body.eval(ext)
 
 
 class Lambda(Callable):
@@ -58,14 +73,10 @@ class Lambda(Callable):
 
     def apply(self, env, *args):
         assert self.names == len(args)
-        env = env.clone()
+        env = env.copy()
         for name, value in zip(self.names, args):
             env.extend(name, value)
         return self.body.eval(env)
-
-
-class Func(A):
-    pass
 
 
 class Env:
@@ -78,10 +89,26 @@ class Env:
     def extend(self, key, value):
         self.kv[key] = value
 
+    def __getitem__(self, val):
+        return self.kv[val]
 
-class S:
-    def __init__(self, body: Union[A, List[S]]):
+    def copy(self):
+        return Env(self.kv)
+
+
+class S(A):
+    def __init__(self, *body):
         self.body = body
+
+    def __getitem__(self, idx):
+        return self.body[idx]
+
+    def __iter__(self):
+        if isinstance(self.body, A):
+            yield self.body
+        else:
+            for item in self.body:
+                yield item
 
     def eval(self, env: Env):
         if isinstance(self.body, A):
@@ -89,11 +116,19 @@ class S:
         valuated = []
         for arg in self.body:
             valuated.append(arg.eval(env))
-        assert isinstance(valuated[0], Callable)
+        assert isinstance(valuated[0], Callable), f"{valuated[0]} is not callable!"
         return valuated[0].apply(*valuated[1:])
 
 
 #   ((lambda (x) (lambda (x) x)) 5)
 
 env = Env()
-print(test.eval(env))
+
+test1 = Let(S(S(Id("x"), Int(5))), Id("x"))
+
+
+test2 = Let(S(S(Id("x"), Int(5)), S(Id("y"), Int(2))), S(AddOp(), Id("y"), Id("x")))
+
+test3
+
+print(test2.eval(env))
